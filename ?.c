5f6caaf381f1c874e5bcf7fe4ed15b60244d6f72
Bobby Jonathan <bobby.cool00763@gmail.com>
2017-10-17 11:48:08 +0700

    Update ?.c

--- ?.c
+++ ?.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define mx 101
 
 double mean(int *data, int n){
-    int m=n,i; float total=0;
+    int i; double total=0;
     for(i=0;i<n;i++){
         total=total+*(data+i);
     }
-    return total/m;
+    return total/n;
 }
 double variance(int *data, int n){
-    int i; double result;
+    int i; double result=0,m=mean(data,n);
     for(i=0;i<n;i++){
-        result=result+pow((*(data+i)-mean(data,n)),2);
+        result=result+pow((*(data+i)-m),2);
     }
     return result/n;
 }
 double stddev(int *data, int n){
-    return sqrt(variance(data,n));
+    double v=sqrt(variance(data,n));
+    return v;
 }
 
 double stat(double (*function)(int *, int), int *data, int n){
@@ -31,14 +31,14 @@ int main(){
 
     scanf("%c", &op);
     scanf("%d",&n);
-    *data=(int*)malloc(n*sizeof(int));
+    data=(int*)malloc(n*sizeof(int));
     for(i=0;i<n;i++){
         scanf("%d",data+i);
     }
     switch (op) {
-        case 'M': printf("%f\n", stat(mean, data, n)); break;
-        case 'V': printf("%f\n", stat(variance, data, n)); break;
-        case 'S': printf("%f\n", stat(stddev, data, n));
+        case 'M': printf("%lf\n", stat(mean, data, n)); break;
+        case 'V': printf("%lf\n", stat(variance, data, n)); break;
+        case 'S': printf("%lf\n", stat(stddev, data, n));
     }
     return 0;
 }
