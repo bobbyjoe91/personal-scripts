de5efd1ac03172c007f6a3f784d12c9a60f4c050
bobbyjoe91 <bobby.cool00763@gmail.com>
2019-04-13 15:19:05 +0700

    updated

--- soundconverter.py
+++ soundconverter.py
@@ -1,5 +1,8 @@
 import os
 cmd = "soundconverter -b -m \"audio/mpeg\" -s \".mp3\" *.mp4"
-video_name = input()
-cmd = cmd.replace("*", video_name)
+cmd2 = "pwd"
+print("You're currently in: ")
+os.system(cmd2)
+video_name = input("Insert path here: ")
+cmd = cmd.replace("*", "\"" + video_name + "\"")
 os.system(cmd)
